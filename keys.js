c57ddc653bb430c6329111fdf621be75fe58d0f7
Carl Vega <carljasonvega@gmail.com>
2018-11-12 18:24:01 -0500

    functioning BandInTown, OMDB, and Spotify

--- keys.js
+++ keys.js
@@ -1,6 +1,12 @@
-console.log("this is loaded");
+exports.bands = {
+  apikey: process.env.bandsKey
+};
 
 exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
+};
+
+exports.omdb = {
+  apikey: process.env.omdbKey
 };
--- liri.js
+++ liri.js
@@ -1,7 +1,11 @@
-var dotenv = require("dotenv").config();
+require("dotenv").config();
 var request = require("request");
+var Spotify = require("node-spotify-api");
+var keys = require("./keys");
 
-//var spotify = new Spotify(keys.spotify);
+var divider = "----------------------";
+
+var spotify = new Spotify(keys.spotify);
 
 var option = process.argv[2];
 var input = process.argv.slice(3).join(" ");
@@ -9,20 +13,21 @@ var input = process.argv.slice(3).join(" ");
 if (option === "concert-this") {
   bandsInTown(input);
 } else if (option === "spotify-this-song") {
-  spotify(input);
+  spotifyAPI(input);
 } else if (option === "movie-this") {
-  omdb(input);
+  omdbAPI(input);
 } else if (option === "do-what-it-says") {
   random(input);
 } else {
   console.log("Typing failed");
 }
-//https://rest.bandsintown.com/artists/madonna/events?app_id=d94b32b3911847a25852f17070a59807
+
 function bandsInTown(artist) {
   var URL =
     "https://rest.bandsintown.com/artists/" +
     artist +
-    "/events?app_id=d94b32b3911847a25852f17070a59807";
+    "/events?app_id=" +
+    keys.bands.bandskey;
 
   request(URL, function(err, response, body) {
     var jsonData = JSON.parse(body)[0];
@@ -40,6 +45,54 @@ function bandsInTown(artist) {
     console.log("\n" + bandsData);
   });
 }
-// function spotify() {}
-// function omdb() {}
+
+function spotifyAPI(track) {
+  spotify.search({ type: "track", query: track }, function(err, data) {
+    if (err) {
+      return console.log("Error occurred: " + err);
+    }
+    const songData = data.tracks.items.map(track =>
+      [
+        `Song: ${track.name}`,
+        `Artist(s): ${track.artists.map(artist => artist.name).join(", ")}`,
+        `Preview: ${track.preview_url || "None"}`,
+        `Album: ${track.album.name}`
+      ].join("\n")
+    );
+    console.log("\n" + songData.join("\n" + divider + "\n"));
+  });
+}
+
+function omdbAPI(movie) {
+  const URL =
+    "http://www.omdbapi.com/?t=" +
+    movie +
+    "&y=&plot=short&r=json&apikey=" +
+    keys.omdb.apikey;
+
+  request(URL, function(error, response, body) {
+    if (error) {
+      return console.error(error);
+    }
+
+    if (body.length < 1) {
+      return console.log("No movies were found!");
+    }
+    const jsonData = JSON.parse(body);
+
+    const movieData = [
+      "Title: " + jsonData.Title,
+      "Year: " + jsonData.Year,
+      "IMDB Rating: " + jsonData.Ratings[0].Value,
+      "Rotten Tomatoes Rating: " + jsonData.Ratings[1].Value,
+      "Country: " + jsonData.Country,
+      "Language: " + jsonData.Language,
+      "Plot Summary: " + jsonData.Plot,
+      "Actor(s): " + jsonData.Actors
+    ].join("\n");
+
+    console.log("\n" + movieData);
+  });
+  // var URL = "http://www.omdbapi.com/?t=" + movie + "&" + ;
+}
 // function random() {}
